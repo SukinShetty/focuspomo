4d4754382d3b2b2528fc71e85dba053e045b8f86
sukin shetty <sukin.shetty8@gmail.com>
2024-08-29 16:52:54 +0530

    created app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import SimpleFocusTimer from '../../components/SimpleFocusTimer'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <SimpleFocusTimer />
+    </main>
+  )
+}
